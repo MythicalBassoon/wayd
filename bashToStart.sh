d5157bd1b39c34d0b6e9475d9f6c0de0095f3d3c
shafiquerashid <shafique.a.rashid@gmail.com>
2016-02-02 14:58:27 -0800

    Updated bash script to contain absolute paths

--- bashToStart.sh
+++ bashToStart.sh
@@ -9,7 +9,8 @@ osascript -e 'tell application "Terminal" to activate' -e 'tell application "Sys
 osascript -e 'tell application "Terminal" to activate' -e 'tell application "System Events" to tell process "Terminal" to keystroke "t" using command down' 
 osascript -e 'tell application "Terminal" to do script "redis-server shutdown" in tab 2 of window 1'
 osascript -e 'tell application "Terminal" to do script "redis-server" in tab 2 of window 1'
-osascript -e 'tell application "Terminal" to do script "nodemon Desktop/HR/final/wayd/Server/server.js" in tab 3 of window 1'
-osascript -e 'tell application "Terminal" to do script "nodemon Desktop/HR/final/wayd/Server/Workers/jobserver.js" in tab 4 of window 1'
-osascript -e 'tell application "Terminal" to do script "node Desktop/HR/final/wayd/Server/Workers/jobqueue.js" in tab 5 of window 1'
-osascript -e 'tell application "Terminal" to do script "open Desktop/HR/final/wayd/wayd.xcworkspace" in tab 6 of window 1'
+osascript -e 'tell application "Terminal" to do script "nodemon /Users/shafiquerashid/HR/wayd/Server/server.js" in tab 3 of window 1'
+osascript -e 'tell application "Terminal" to do script "nodemon /Users/shafiquerashid/HR/wayd/Server/Workers/jobserver.js" in tab 4 of window 1'
+osascript -e 'tell application "Terminal" to do script "node /Users/shafiquerashid/HR/wayd/Server/Workers/jobqueue.js" in tab 5 of window 1'
+osascript -e 'tell application "Terminal" to do script "open /Users/shafiquerashid/HR/wayd/wayd.xcworkspace" in tab 6 of window 1'
+
