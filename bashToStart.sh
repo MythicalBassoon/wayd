175a607775bcf0ea22f16298be60671e8dacd814
Richard Castro <rscastro13@gmail.com>
2016-02-02 17:57:01 -0800

    basic docker skeleton

--- bashToStart.sh
+++ bashToStart.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-dropdb wayd
 createdb wayd
 osascript -e 'tell application "Terminal" to activate' -e 'tell application "System Events" to tell process "Terminal" to keystroke "t" using command down' 
 osascript -e 'tell application "Terminal" to activate' -e 'tell application "System Events" to tell process "Terminal" to keystroke "t" using command down'
